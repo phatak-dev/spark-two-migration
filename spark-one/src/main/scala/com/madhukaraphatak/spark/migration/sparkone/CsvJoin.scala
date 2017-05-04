83a401e4eb2b012a6227bdb4bbd8b5ec0f8f6b83
madhu <phatak.dev@gmail.com>
2017-05-04 20:53:59 +0530

    added csv join example

---
+++ spark-one/src/main/scala/com/madhukaraphatak/spark/migration/sparkone/CsvJoin.scala
@@ -0,0 +1,26 @@
+package com.madhukaraphatak.spark.migration.sparkone
+
+import org.apache.spark.sql._
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object CsvJoin {
+
+  def main(args: Array[String]) = {
+
+    val sparkConf = new SparkConf().setMaster("local").setAppName("mapexample")
+    val sparkContext = new SparkContext(sparkConf)
+    val sqlContext = SQLContext.getOrCreate(sparkContext)
+
+    val loadedDf = sqlContext.read.format("com.databricks.spark.csv").
+    option("header", "true").load(args(0))
+
+    val miniDf = loadedDf.limit(10000)
+
+    val joinedDf = loadedDf.join(miniDf, loadedDf.col("Year") === miniDf.col("Year"))
+
+    println(joinedDf.count)
+
+    sparkContext.stop()
+  }
+}
---
+++ spark-two/src/main/scala/com/madhukaraphatak/spark/migration/sparktwo/CsvJoin.scala
@@ -0,0 +1,17 @@
+package com.madhukaraphatak.spark.migration.sparktwo
+
+import org.apache.spark.sql._
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object CsvJoin {
+
+  def main(args: Array[String]):Unit = {
+
+    val sparkSession = SparkSession.builder.master("local").appName("mapexample").getOrCreate()
+    val loadedDf = sparkSession.read.format("csv").option("header", "true").load(args(0))
+    val miniDf = loadedDf.limit(10000)
+    val joinedDf = loadedDf.join(miniDf, loadedDf.col("Year") === miniDf.col("Year"))
+    joinedDf.count
+  }
+}
